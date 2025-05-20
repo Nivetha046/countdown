a425f182d9f3af86991ef14ed17fa9d69e4265f4
Nivetha046 <nivethanav46@gmail.com>
2025-05-20 14:36:28 +0530

    Add files via upload

---
+++ register.jsx
@@ -0,0 +1,19 @@
+import { Link , useNavigate } from "react-router-dom"
+import Login from "./Login"
+
+
+const Register = () =>{
+    const navigate=useNavigate()
+
+    const handileClick=()=>{
+        console.log("Handile click function called")
+        navigate("/login")
+    }
+    return(
+        <div>
+            <h1> This is Register page.</h1>
+            <button onClick={handileClick}>Login</button>
+        </div>
+    )
+}
+export default Register
